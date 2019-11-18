70890229d5e4963940010473ba0f11cd52ce7879
ezajacthan <ezajacthan@F1092-18-2106>
2019-11-18 15:07:45 -0600

    Classwork 11-18

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -27,5 +27,43 @@ namespace Movies
         }
 
         public List<Movie> All { get { return movies; } }
+
+        public List<Movie> SearchAndFilter (string searchString, List<string> rating)
+        {
+            //default search coverage
+            if (searchString == null && rating.Count == 0) return All;
+
+            List<Movie> returnList = new List<Movie>();
+            foreach(Movie mov in movies)
+            {
+                //Search with ratings
+                if (searchString != null && rating.Count > 0)
+                {
+                    if (mov.Title != null && mov.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase) && rating.Contains(mov.MPAA_Rating))
+                    {
+                        returnList.Add(mov);
+                    }
+                }
+                //Search only
+                else if (searchString != null)
+                {
+                    if (mov.Title != null && mov.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        returnList.Add(mov);
+                    }
+                }
+                //Ratings only
+                else if (rating.Count > 0)
+                {
+                    if (rating.Contains(mov.MPAA_Rating))
+                    {
+                        returnList.Add(mov);
+                    }
+                }
+
+            }
+
+            return returnList;
+        }
     }
 }
